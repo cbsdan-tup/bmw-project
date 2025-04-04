be92a3c24d4bd4e29b7c5f9433fec48f4aaa8635
Daniel Cabasa <cabasadaniel1@gmail.com>
2025-04-05 04:37:38 +0800

    uninstalled unneeded packages, setup production build

--- mobile/services/baseUrl.js
+++ mobile/services/baseUrl.js
@@ -1,11 +1,8 @@
 const environments = {
   development: {
-    androidEmulator: "http://192.168.1.13:4000/api/v1",
-    iOSSimulator: "http://localhost:4000/api/v1",
-    deviceLocal: "http://192.168.1.17:4000/api/v1",
-    staging: "https://staging-api.bmw-rentals.com/api/v1",
+    deviceLocal: "http://192.168.68.101:4000/api/v1"
   },
-  production: "https://api.bmw-rentals.com/api/v1",
+  production: "https://bmw-project-514926560822.us-central1.run.app/api/v1",
 };
 
 const getBaseUrl = () => {
